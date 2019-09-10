ecce382543e7279320cfd36200a7ef6d1663d198
unknown <Uku.Soome@gmail.com>
2019-09-10 12:26:19 +0300

    robot controls

--- MainBoard.py
+++ MainBoard.py
@@ -1,15 +1,38 @@
 import serial
-ser = serial.Serial('/dev/ttyUSB0')  # open serial port
+import cv2
+ser = serial.Serial('/dev/ttyUSB0')
 
 
-ser.write(sd:10:10:0:0)     # write a string
+ser.write("sd:10:10:0:0").encode("utf-8")
 
-for i in range(300):
-    if i%30==0:
-        ser.write(sd:10:10:0:0)
-    if 0xFF == ord('q'):
-        ser.write(sd:0:0:0:0)
+
+def motorsOff():
+    ser.write("sd:0:0:0:0").encode("utf-8")
+
+def driveForward():
+    ser.write("sd:10:10:0:0").encode("utf-8")
+
+def driveLeft():
+    ser.write("sd:10:0:10:0").encode("utf-8")  ## test it
+
+def driveRight():
+    ser.write("sd:0:10:10:0").encode("utf-8") ## test it
+
+def driveBackwards():
+    ser.write("sd:-10:-10:0:0").encode("utf-8")
+while (1):
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('w'):
+        driveForward()
+    if key == ord("s"):
+        driveBackwards()
+    if key == ord("a"):
+        driveLeft()
+    if key == ord("d"):
+        driveRight()
+    if key == ord('q'):
+        motorsOff()
         break
 
-ser.write(sd: 0:0: 0:0)
+motorsOff()
 ser.close()
